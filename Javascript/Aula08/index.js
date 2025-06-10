943dec839a6c60a0cc650ca767269a511cb8cb43
LUIS FERNANDO GUIMARAES SANTOS <luis.fgsantos6@senacsp.edu.br>
2025-06-09 21:36:07 -0300

    aula08 javascript

--- Javascript/Aula08/index.js
+++ Javascript/Aula08/index.js
@@ -108,60 +108,21 @@
 
 // ATIVIDADE 1
 
-// let produtos = [
-//     {nome: 'Leite', estoque: 6},
-//     {nome: 'Ovos', estoque: 6},
-//     {nome: 'Cuscuz', estoque: 6}
-// ]
-
-// let continuar = true;
-
-// while(continuar){
-//     let lista = 'Produtos:\n\n'
-//     for (let i = 0; i < produtos.length; i++){
-//         lista += `${i+1}. ${produtos[i].nome} - ${produtos[i].estoque} Un.\n`
-//     }
-
-//     lista += "\nDigite o n° do produto que deseja remover ou 'sair' para encerrar o programa:";
-//     let escolha = prompt(lista);
-
-//     if (escolha === null || escolha.toLocaleLowerCase() === "sair") {
-//         continuar = false;
-//     }
-
-//     let indice = Number(escolha) - 1;
-
-//     if (!isNaN(indice) && indice >= 0 && indice < produtos.length){
-
-//         produtos[indice].estoque--;
-//         alert = (`1 unidade de ${produtos[indice].nome} removido`);
-
-//     }else {
-
-//         alert("Opção inválida. Tente novamente!")
-
-//     }
-
-// }
-
-// atividade 2
-
-let biblioteca =  [
-    {livro: 'Harry Potter', qtd: 10},
-    {livro: 'Senhos dos Anéis', qtd: 20},
-    {livro: 'As vantagens de ser invisível',qtd: 5}
+let produtos = [
+    {nome: 'Leite', estoque: 6},
+    {nome: 'Ovos', estoque: 5},
+    {nome: 'Cuscuz', estoque: 3}
 ]
 
 let continuar = true;
 
-while (continuar){
-    let lista = 'Biblioteca Senac:\n\n'
-    for (let i = 0; i < biblioteca.length; i++){
-        lista += `${i+1}. ${biblioteca[i].livro} - ${biblioteca[i].qtd} Un.\n`
+while(continuar){
+    let lista = 'Produtos:\n\n'
+    for (let i = 0; i < produtos.length; i++){
+        lista += `${i+1}. ${produtos[i].nome} - ${produtos[i].estoque} Un.\n`
     }
 
-    lista += "\nDigite o n° do livro que deseja alterar ou 'sair' para encerrar o programa:";
-
+    lista += "\nDigite o n° do produto que deseja remover ou 'sair' para encerrar o programa:";
     let escolha = prompt(lista);
 
     if (escolha === null || escolha.toLocaleLowerCase() === "sair") {
@@ -171,26 +132,66 @@ while (continuar){
 
     let indice = Number(escolha) - 1;
 
-    if (!isNaN(indice) && indice >= 0 && indice < biblioteca.length){
-        let novaQtd = parseFloat(prompt(`Digite uma nova quantidade para "${biblioteca[indice].livro}"`));
+    if (!isNaN(indice) && indice >= 0 && indice < produtos.length){
 
-        if (!isNaN(novaQtd)) {
-            biblioteca[indice].qtd = novaQtd;
-            alert(`Quantidade do livro "${biblioteca[indice].livro}" atualizado para ${novaQtd} unidades.`)
-        }
-        else {
-            alert("Quantidade inválida. Tente novamente!")
-        }
-    }
-    else {
+        produtos[indice].estoque--;
+        alert = (`1 unidade de ${produtos[indice].nome} removido`);
+
+    }else {
+
         alert("Opção inválida. Tente novamente!")
+
     }
+
 }
 
-let resumo =  "Biblioteca atualizada:\n\n"
+// atividade 2
 
-for(let b of biblioteca){
-    resumo += `${b.livro}: ${b.qtd} Un.\n`;
-}
+// let biblioteca =  [
+//     {livro: 'Harry Potter', qtd: 10},
+//     {livro: 'Senhos dos Anéis', qtd: 20},
+//     {livro: 'As vantagens de ser invisível',qtd: 5}
+// ]
 
-alert(resumo);
+// let continuar = true;
+
+// while (continuar){
+//     let lista = 'Biblioteca Senac:\n\n'
+//     for (let i = 0; i < biblioteca.length; i++){
+//         lista += `${i+1}. ${biblioteca[i].livro} - ${biblioteca[i].qtd} Un.\n`
+//     }
+
+//     lista += "\nDigite o n° do livro que deseja alterar ou 'sair' para encerrar o programa:";
+
+//     let escolha = prompt(lista);
+
+//     if (escolha === null || escolha.toLocaleLowerCase() === "sair") {
+//         continuar = false;
+//         break;
+//     }
+
+//     let indice = Number(escolha) - 1;
+
+//     if (!isNaN(indice) && indice >= 0 && indice < biblioteca.length){
+//         let novaQtd = parseFloat(prompt(`Digite uma nova quantidade para "${biblioteca[indice].livro}"`));
+
+//         if (!isNaN(novaQtd)) {
+//             biblioteca[indice].qtd = novaQtd;
+//             alert(`Quantidade do livro "${biblioteca[indice].livro}" atualizado para ${novaQtd} unidades.`)
+//         }
+//         else {
+//             alert("Quantidade inválida. Tente novamente!")
+//         }
+//     }
+//     else {
+//         alert("Opção inválida. Tente novamente!")
+//     }
+// }
+
+// let resumo =  "Biblioteca atualizada:\n\n"
+
+// for(let b of biblioteca){
+//     resumo += `${b.livro}: ${b.qtd} Un.\n`;
+// }
+
+// alert(resumo);
