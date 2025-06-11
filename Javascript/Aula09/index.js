95ba3cf85467da2e057a5ed7ad36c78b3af85e27
LUIS FERNANDO GUIMARAES SANTOS <luis.fgsantos6@senacsp.edu.br>
2025-06-10 22:06:23 -0300

    aula 09 javascript

--- Javascript/Aula09/index.js
+++ Javascript/Aula09/index.js
@@ -25,44 +25,78 @@
 
 // atividade 4
 
-let alunos = [
-    {
-        nome: 'Luis',
-        idade: 28,
-        notas: [8,9,7.5]
-    },
-    {
-        nome: 'Bruno',
-        idade: 29,
-        notas: [9,6,10]
-    },
-    {
-        nome:'Guilherme',
-        idade: 25,
-        notas: [5,5,5]
-    }
-]
+// let alunos = [
+//     {
+//         nome: 'Luis',
+//         idade: 28,
+//         notas: [8,9,7.5]
+//     },
+//     {
+//         nome: 'Bruno',
+//         idade: 29,
+//         notas: [9,6,10]
+//     },
+//     {
+//         nome:'Guilherme',
+//         idade: 25,
+//         notas: [5,5,5]
+//     }
+// ]
 
-let txt = "";
-let totalNotas = 0, media = 0;
+// let txt = "";
+// let totalNotas = 0, media = 0;
 
-for (i = 0; i < alunos.length; i++) {
-    for (j = 0; j < alunos[i].notas.length; j++) {
-        totalNotas += alunos[i].notas[j];
-    }
+// for (i = 0; i < alunos.length; i++) {
+//     for (j = 0; j < alunos[i].notas.length; j++) {
+//         totalNotas += alunos[i].notas[j];
+//     }
 
-    media = totalNotas / 3;
-    txt += `${alunos[i].nome} - Média ${media.toFixed(2)} - Situação: `;
+//     media = totalNotas / alunos[i].notas.length;
+//     txt += `${alunos[i].nome} - Média ${media.toFixed(2)} - Situação: `;
     
 
-    if (media < 7) {
-        txt += "Reprovado\n\n";
-    }else {
-        txt += "Aprovado\n\n";
+//     if (media < 7) {
+//         txt += "Reprovado\n\n";
+//     }else {
+//         txt += "Aprovado\n\n";
+//     }
+
+//     totalNotas = 0;
+//     media = 0;
+// }
+
+// alert(txt);
+
+
+// Catálogo Filmes
+
+let filmes = [
+    {titulo: "Vingadores", genero:"Aao"},
+    {titulo: "Up - Altas aventuras", genero:"Animaçao"},
+    {titulo: "Como eu era antes de você", genero:"Romance"},
+    {titulo: "Cruela", genero:"Comedia"},
+    {titulo: "Interestelar", genero:"Ficçao Cientifica"}
+]
+
+let generoBuscado = prompt("Digite gênero de filme que você deseja assistir: ")
+
+if(generoBuscado){
+    let encontrou = false;
+    let resultado = `Filmes do genero "${generoBuscado}":\n\n`;
+    
+    for (i = 0; i < filmes.length; i++){
+        if (filmes[i].genero.toLocaleLowerCase() === generoBuscado.toLocaleLowerCase()){
+            resultado += `- ${filmes[i].titulo} \n`;
+            encontrou = true;
+        }
     }
-
-    totalNotas = 0;
-    media = 0;
+    if (encontrou){
+        alert(resultado)
+    }
+    else {
+        alert(`Não encontramos filmes do gênero "${generoBuscado}" 😕`);
+    }
 }
-
-alert(txt);
+else {
+    alert("Nenhum gênero informado");
+}
