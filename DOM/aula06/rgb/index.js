86c5051e7b2a7a68fd66a4ddba5cd656a0c84859
LUIS FERNANDO GUIMARAES SANTOS <luis.fgsantos6@senacsp.edu.br>
2025-06-27 22:08:33 -0300

    aula06 portifolio

---
+++ DOM/aula06/rgb/index.js
@@ -0,0 +1,26 @@
+function mudarCor(){
+    const textRed = document.getElementById('idRed')
+    const textGreen = document.getElementById('idGreen')
+    const textBlue = document.getElementById('idBlue')
+
+    const red = document.getElementById('inputRed').value;
+    const green = document.getElementById('inputGreen').value;
+    const blue = document.getElementById('inputBlue').value;
+
+    let cor = `rgb(${red},${green},${blue})`;
+
+    const rgb = document.getElementById('rgb')
+    rgb.style.color = cor;
+
+    document.getElementById('container').style.backgroundColor = cor;
+
+    textRed.value = red;
+    textGreen.value = green;
+    textBlue.value = red;
+}
+
+document.getElementById('inputRed').addEventListener('input', mudarCor)
+document.getElementById('inputGreen').addEventListener('input', mudarCor)
+document.getElementById('inputBlue').addEventListener('input', mudarCor)
+
+window.onload = mudarCor;
